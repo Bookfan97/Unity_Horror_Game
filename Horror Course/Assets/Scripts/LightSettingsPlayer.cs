4ffe055b39257ba04f83ef2a975b20e8e9b630b7
Noah Day <sheltielover432@gmail.com>
2020-08-06 23:32:50 -0400

    Switching Cameras

---
+++ Horror Course/Assets/Scripts/LightSettingsPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+public class LightSettingsPlayer : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume currentVolume;
+    [SerializeField] private PostProcessProfile Standard;
+    [SerializeField] private PostProcessProfile NightVision;
+    [SerializeField] private GameObject NightVisionHUD;
+    private bool NightVisionActive = false;
+
+    private void Start()
+    {
+        NightVisionHUD.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (NightVisionActive == false)
+            {
+                NightVisionActive = true;
+                currentVolume.profile = NightVision;
+                NightVisionHUD.gameObject.SetActive(true);
+            }
+            else
+            {
+                NightVisionActive = false;
+                currentVolume.profile = Standard;
+                NightVisionHUD.gameObject.SetActive(false);
+            }
+        }
+    }
+}
