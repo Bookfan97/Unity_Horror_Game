5eab280fa3bcfbc5cbb62f22d8461428f72dbe12
Noah Day <sheltielover432@gmail.com>
2020-08-07 01:09:17 -0400

    Lighthouse

---
+++ Horror Course/Assets/Scripts/LighthouseRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LighthouseRotate : MonoBehaviour
+{
+    [SerializeField] private GameObject LighthouseBeacon;
+    [SerializeField] private float Speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LighthouseBeacon.transform.Rotate(0.0f, Speed, 0.0f, Space.World);
+    }
+}
