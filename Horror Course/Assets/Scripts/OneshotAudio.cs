7a7b2dd63319bc597a44823565c0ddac8ed9d243
Noah Day <sheltielover432@gmail.com>
2020-08-08 19:28:47 -0400

    Reorganizing

---
+++ Horror Course/Assets/Scripts/OneshotAudio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneshotAudio : MonoBehaviour
+{
+    private AudioSource AudioSource;
+    private Collider attachedCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+        attachedCollider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioSource.Stop();
+            AudioSource.Play();
+            attachedCollider.enabled = false;
+        }
+    }
+}
