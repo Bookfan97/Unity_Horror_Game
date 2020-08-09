2fc608d8a3e442f451f231dac5045900724bda49
Noah Day <sheltielover432@gmail.com>
2020-08-08 20:49:36 -0400

    HealthBar

---
+++ Horror Course/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    [SerializeField] Slider healthSlider;
+    public Image Fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSlider.minValue = 0f;
+        healthSlider.maxValue = 100;
+        healthSlider.value = SaveScript.PlayerHealth; 
+        healthSlider.value = SaveScript.PlayerHealth;
+        Fill.color = Color.Lerp(Color.red, Color.green, (float) SaveScript.PlayerHealth / 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SaveScript.healthChanged)
+        {
+            SaveScript.healthChanged = false;
+            healthSlider.value = SaveScript.PlayerHealth;
+            Fill.color = Color.Lerp(Color.red, Color.green, (float) SaveScript.PlayerHealth / 100);
+        }
+    }
+}
---
+++ Horror Course/Assets/Scripts/SaveScript.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveScript : MonoBehaviour
+{
+    public static int PlayerHealth = 50;
+    public static bool healthChanged = false;
+}
