c06802edee769176fa4b946d621e74e3ce31c6e7
Noah Day <sheltielover432@gmail.com>
2020-08-08 23:49:10 -0400

    Inventory Menu

---
+++ Horror Course/Assets/Scripts/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private GameObject InventoryMenu;
+
+    private bool InventoryActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InventoryMenu.gameObject.SetActive(false);
+        InventoryActive = false;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            InventoryActive = !InventoryActive;
+            InventoryMenu.gameObject.SetActive(InventoryActive);
+            Cursor.visible = InventoryActive;
+            if (InventoryActive)
+            {
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+            
+        }
+    }
+}
