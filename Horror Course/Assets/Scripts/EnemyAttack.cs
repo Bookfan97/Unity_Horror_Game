db5ccb19d2479cce5677a813eb3937d4d660876d
Noah Day <sheltielover432@gmail.com>
2020-08-19 22:11:20 -0400

    Enemy Attack UI and Sounds

--- Horror Course/Assets/Scripts/EnemyAttack.cs
+++ Horror Course/Assets/Scripts/EnemyAttack.cs
@@ -22,6 +22,7 @@ public class EnemyAttack : MonoBehaviour
     [SerializeField] private float attackRotateSpeed = 2.0f;
     [SerializeField] private float CheckTime = 3.0f;
     [SerializeField] private GameObject ChaseMusic;
+    [SerializeField] private GameObject HurtUI;
    
     // Start is called before the first frame update
     void Start()
@@ -43,13 +44,13 @@ public class EnemyAttack : MonoBehaviour
                 blocked = NavMesh.Raycast(transform.position, Player.position, out Hit, NavMesh.AllAreas);
                 if (blocked == false)
                 {
-                    Debug.Log("I can see you, you may want to run");
+                    //Debug.Log("I can see you, you may want to run");
                     runToPlayer = true;
                     FailedChecks = 0;
                 }
                 if (blocked == true)
                 {
-                    Debug.Log("Missed it by that much");
+                   // Debug.Log("Missed it by that much");
                     runToPlayer = false;
                     _animator.SetInteger("State", 1);
                     FailedChecks++;
@@ -66,17 +67,19 @@ public class EnemyAttack : MonoBehaviour
             {
                 _navMeshAgent.isStopped = false;
                 _animator.SetInteger("State", 2);
-                _navMeshAgent.acceleration = 180;
+                _navMeshAgent.acceleration = 24;
                 _navMeshAgent.SetDestination(Player.position);
                 _navMeshAgent.speed = chaseSpeed;
+                HurtUI.gameObject.SetActive(false);
             }
 
             if (distanceToPlayer < AttackDistance - 0.5f)
             {
                 _navMeshAgent.isStopped = true;
-                Debug.Log("Finish him!");
+               // Debug.Log("Finish him!");
                 _animator.SetInteger("State", 3);
-                _navMeshAgent.acceleration = 24;
+                _navMeshAgent.acceleration = 180;
+                HurtUI.gameObject.SetActive(true);
                 Vector3 Position = (Player.position - Enemy.transform.position).normalized;
                 Quaternion PostionRotation = Quaternion.LookRotation(new Vector3(Position.x,0, Position.z));
                 Enemy.transform.rotation = Quaternion.Slerp(Enemy.transform.rotation, PostionRotation, Time.deltaTime);
---
+++ Horror Course/Assets/Scripts/EnemyWeaponDamage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeaponDamage : MonoBehaviour
+{
+    [SerializeField] private int WeaponDamage = 10;
+    [SerializeField] private Animator hurtAnim;
+    [SerializeField] private AudioSource stabSound;
+    private bool hitActive = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (hitActive == false)
+            {
+                hitActive = true;
+                hurtAnim.SetTrigger("Hurt");
+                SaveScript.PlayerHealth -= WeaponDamage;
+                SaveScript.healthChanged = true;
+                stabSound.Play();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (hitActive == true)
+            {
+                hitActive = false;
+            }
+        }
+    }
+}
